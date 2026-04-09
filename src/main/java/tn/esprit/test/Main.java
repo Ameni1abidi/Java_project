996b2d2332074d2dc027df0c79834d9dd0ba020d
mariemrahmouni <Mariem.Rahmouni@esprit.tn>
2026-04-09 17:35:13 +0100

    Création de l'entité Categorie avec son CRUD.

--- src/main/java/tn/esprit/test/Main.java
+++ src/main/java/tn/esprit/test/Main.java
@@ -1,10 +1,52 @@
 package tn.esprit.test;
 
+import tn.esprit.entities.categorie;
+import tn.esprit.entities.resources;
+import tn.esprit.services.CategoryService;
+import tn.esprit.services.ResourceService;
+
 public class Main {
     public static void main(String[] args) {
+        // Service Categorie
+        CategoryService categoryService = new CategoryService();
 
-        System.out.printf("Hello and welcome!");
+        // CREATE Categorie
+        categorie c = new categorie("Categorie Test");
+        categoryService.add(c);
+        System.out.println("categorie added");
 
+        // READ Categorie
+        //categoryService.getAll().forEach(System.out::println);
 
+        // UPDATE Categorie
+        c.setNom("Categorie Modifiee");
+        categoryService.update(c);
+        System.out.println("categorie modified");
+        // DELETE Categorie
+        // categoryService.delete(c.getId());
+
+        // Service Ressource
+        ResourceService resourceService = new ResourceService();
+
+        // CREATE Ressource
+        resources r = new resources(
+                "Ressource Test",
+                "Contenu Test",
+                c.getId()
+        );
+        resourceService.add(r);
+        System.out.println("ressource added");
+
+        // READ Ressource
+        //resourceService.getAll().forEach(System.out::println);
+
+        // UPDATE Ressource
+        r.setTitre("Ressource Modifiee");
+        r.setContenu("Contenu Modifie");
+        r.setCategorieId(c.getId());
+        resourceService.update(r);
+        System.out.println("ressource modified");
+        // DELETE Ressource
+        // resourceService.delete(r.getId());
     }
 }
