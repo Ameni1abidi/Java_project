4c9c2f140c6531d1e62f5ca86c9c557984f22232
Ameni Abidi <amenizina12@gmail.com>
2026-04-10 13:46:22 +0100

    correction main et integration

---
+++ src/main/java/tn/esprit/controllers/EvaluationController.java
@@ -0,0 +1,18 @@
+package tn.esprit.controllers;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import tn.esprit.entities.Evaluation;
+import tn.esprit.services.EvaluationService;
+
+public class EvaluationController {
+
+
+    @FXML
+    public void initialize() {
+        System.out.println("INIT OK");
+    }
+
+}
