23b62af7f2a624a6d159aebf2e4755eee46509f6
mariemrahmouni <Mariem.Rahmouni@esprit.tn>
2026-04-10 14:58:22 +0100

    creation l'interface de categorie

--- src/main/java/tn/esprit/utils/MainFX.java
+++ src/main/java/tn/esprit/utils/MainFX.java
@@ -1,25 +1,24 @@
-package tn.esprit.test;
+package tn.esprit.utils;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.application.Application;
 
-public class mainFX extends Application {
-
+public class MainFX extends Application {
+    @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/views/EvaluationView.fxml")
-        );
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/categorieView.fxml"));
         Scene scene = new Scene(loader.load());
-        stage.setTitle("EduFlex - Résultats");
+
+        stage.setTitle("Gestion des catégories");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
 }
