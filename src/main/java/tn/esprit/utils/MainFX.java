2196770ac36c363edc4df0cf545f60532570c86e
mariemrahmouni <Mariem.Rahmouni@esprit.tn>
2026-04-12 12:12:47 +0100

    ajouter interface

--- src/main/java/tn/esprit/utils/MainFX.java
+++ src/main/java/tn/esprit/utils/MainFX.java
@@ -1,24 +1,32 @@
 package tn.esprit.utils;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
 
 public class MainFX extends Application {
-    @Override
+
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/CategorieList.fxml"));
-        Scene scene = new Scene(loader.load());
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/EvaluationView.fxml")
+        );
 
-        stage.setTitle("Gestion des catégories");
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("EduFlex - Résultats");
         stage.setScene(scene);
         stage.show();
     }
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/forum.fxml"));
+    Scene scene = new Scene(loader.load(), 600, 500);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+        stage.setTitle("EduFlex Forum");
+        stage.setScene(scene);
+        stage.show();
+}
 
+public static void main(String[] args) {
+    launch();
+}
 }
