4d02a68b8b51b519b20af088712411ec560683a1
Aziz Abbes <medazizabbes@gmail.com>
2026-04-12 12:36:19 +0100

    update tous les interaces

--- src/main/java/tn/esprit/utils/MainFX.java
+++ src/main/java/tn/esprit/utils/MainFX.java
@@ -9,16 +9,16 @@ public class MainFX extends Application {
 
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/CategorieList.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ajouterCours.fxml"));
+        /*FXMLLoader loader = new FXMLLoader(getClass().getResource("/CategorieList.fxml"));
+        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/ajouterCours.fxml"));
         Scene scene = new Scene(loader.load());
 
         stage.setTitle("Ajouter Cours");
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
 
 
-        /*FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/forum.fxml"));
+        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/forum.fxml"));
         Scene scene2 = new Scene(loader1.load(), 600, 500);
 
         stage.setTitle("EduFlex Forum");
