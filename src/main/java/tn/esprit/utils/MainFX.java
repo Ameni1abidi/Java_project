1079a4f33c00ccc21f9e4dd4a337926122fe742b
mariemrahmouni <Mariem.Rahmouni@esprit.tn>
2026-04-12 13:19:03 +0100

    controle de saisie pour le categorie

--- src/main/java/tn/esprit/utils/MainFX.java
+++ src/main/java/tn/esprit/utils/MainFX.java
@@ -10,9 +10,8 @@ public class MainFX extends Application {
     public void start(Stage stage) throws Exception {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/CategorieList.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ajouterCours.fxml"));
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("/ajouterCours.fxml"));
         Scene scene = new Scene(loader.load());
-
         stage.setTitle("Ajouter Cours");
         stage.setScene(scene);
         stage.show();
